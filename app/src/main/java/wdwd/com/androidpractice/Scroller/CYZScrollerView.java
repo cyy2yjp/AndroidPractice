8f89a2fecaa0b02a160903de3bb71706b3a3f712
tom <chenyongyong@wdwd.com>
2016-11-15 15:17:03 +0800

    scroller

---
+++ app/src/main/java/wdwd/com/androidpractice/Scroller/CYZScrollerView.java
@@ -0,0 +1,133 @@
+package wdwd.com.androidpractice.Scroller;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.widget.Scroller;
+
+/**
+ * Created by tomchen on 16/11/14.
+ */
+
+public class CYZScrollerView extends ViewGroup {
+
+    private final String TAG = getClass().getSimpleName();
+    private Scroller mScroller;
+    private float mTouchSlop;
+
+    private float mXDown;
+    private float mXMove;
+    private float mXLastMove;
+    private int leftBorder;
+    private int rightBorder;
+    private int orginScrollX;
+    private int orginScrollY;
+
+    public CYZScrollerView(Context context) {
+        super(context);
+        initView();
+    }
+
+    public CYZScrollerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    private void initView() {
+        mScroller = new Scroller(getContext());
+        mTouchSlop = ViewConfiguration.get(getContext()).getScaledTouchSlop();
+        orginScrollX = getScrollX();
+        orginScrollY = getScrollY();
+        setClickable(true);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View childView = getChildAt(i);
+            //为ScrollerLayout 中的每一个子控件测量大小
+            measureChild(childView, widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        if (changed) {
+            int childCount = getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                View childView = getChildAt(i);
+                //为ScrollerLayout 中的每一个自控件在水平方向上进行布局
+                childView.layout(i * childView.getMeasuredWidth(), 0, (i + 1) * childView.getMeasuredWidth(), childView.getMeasuredHeight());
+            }
+            leftBorder = getChildAt(0).getLeft();
+            rightBorder = getChildAt(getChildCount() - 1).getRight();
+        }
+    }
+
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_UP:
+                mXDown = ev.getRawX();
+                mXLastMove = mXDown;
+                break;
+            case MotionEvent.ACTION_DOWN:
+                mXMove = ev.getRawX();
+                float diff = Math.abs(mXMove - mXDown);
+                mXLastMove = mXMove;
+
+                if (diff > mTouchSlop) {
+                    return true;
+                }
+                break;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                mXMove = event.getRawX();
+                int scrolledX = (int) (mXLastMove - mXMove);
+                Log.i(TAG, String.format("getScrollX() + scrolledX < leftBorder %s n", (getScrollX() + scrolledX < leftBorder)));
+                if (getScrollX() + scrolledX < leftBorder) {
+                    scrollTo(leftBorder, 0);
+                    return true;
+                } else if (getScrollX() + getWidth() + scrolledX > rightBorder) {
+                    Log.i(TAG, String.format("getScrollX() + getWidth() + scrolledX > rightBorder %s n", (getScrollX() + getWidth() + scrolledX > rightBorder)));
+                    scrollTo(rightBorder - getWidth(), 0);
+                    return true;
+                }
+                Log.i(TAG, String.format("scrolledx %s", scrolledX));
+                scrollBy(scrolledX, 0);
+                mXLastMove = mXMove;
+                break;
+            case MotionEvent.ACTION_UP:
+                //当手指抬起时,根据当前的滚动值来判定滚动到哪个子控件的界面
+                int targetIndex = (getScrollX() + getWidth() / 2) / getWidth();
+                int dx = targetIndex * getWidth() - getScrollX();
+                //第二步,调用startScroll() 方法来初始化滚动数据并刷新界面
+                mScroller.startScroll(getScrollX(), 0, dx, 0);
+                invalidate();
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if (mScroller.computeScrollOffset()) {
+            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+            postInvalidate();
+        }
+    }
+}
