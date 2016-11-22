0830040a5b97eefaf40da7ce3e8f9f49861c9aa4
tom <chenyongyong@wdwd.com>
2016-11-22 18:42:33 +0800

    沉侵式交互导航栏

--- app/src/main/java/wdwd/com/androidpractice/MainActivity.java
+++ app/src/main/java/wdwd/com/androidpractice/MainActivity.java
@@ -1,7 +1,9 @@
 package wdwd.com.androidpractice;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 /**
  * 大致使用是这样的:
@@ -16,7 +18,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            //透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //透明导航栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+
+        getSupportActionBar().hide();
     }
-
-
 }
